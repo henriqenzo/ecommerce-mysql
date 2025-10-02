5e746caafab6b11f124261372450de31bb8db614
Escorcio777 <escorciotrabalho@gmail.com>
2025-10-01 22:51:36 -0300

    inseri os dados nas tabelas

--- ecommerce_db.sql
+++ ecommerce_db.sql
@@ -91,3 +91,43 @@ create table if not exists ItemPedido (
     constraint fk_pedido_item foreign key (pedido_id) references Pedido(id) on update cascade on delete cascade,
     constraint fk_produto_item foreign key (produto_id) references Produto(id) on update cascade on delete restrict
 );
+
+/* ----- Inserção de dados ----- */
+
+insert into Cliente (pnome, sobrenome, email, senha, telefone) values 
+("Memphis", "Depay", "m.depay@gmail.com", SHA2(concat('salt', 'minhaSenha'), 256), "61984378860"),
+("Cassio", "Ramos", "cassio.r@gmail.com", SHA2(concat('salt', 'senhaMuitoSegura'), 256), "61993664519");
+
+insert into Endereco (cliente_id, rua, numero, bairro, cidade, estado, cep) values 
+(1, "Rua 2", "92", "SHVP", "Brasília", "DF", "72115025"),
+(1, "CNB 14", "54", "Taguatinga Norte", "Brasília", "DF", "72125050"),
+(2, "QNE 5", "7", "Taguatinga Norte", "Brasília", "DF", "72110027");
+
+insert into Pedido (cliente_id, endereco_id, valor_total) values 
+(1, 1, 139.90),
+(1, 2, 239.80),
+(2, 3, 299.70);
+
+insert into Pagamento (pedido_id, status_pagamento) values 
+(1, "Aprovado"),
+(2, "Recusado");
+
+insert into Pix (pagamento_id, chave_pix) values 
+(1, "07289851102");
+
+insert into Cartao (pagamento_id, numero, validade, cvc) values 
+(2, "50505050101010", "12-30", "134");
+
+insert into Categoria (nome, descricao) values 
+("Casacos", "Roupas para usar nessa temporada de frio!"),
+("Camisetas", "Camisetas básicas, oversized e slim");
+
+insert into Produto (categoria_id, nome, descricao, imagem_url, preco, tamanho, qtd_estoque) values 
+(1, "Moletom Canguru", "Lindo moletom com capuz e bolso canguru", "https://img.ltwebstatic.com/v4/j/spmp/2025/08/18/e6/1755515893612011cef33a8a67e4fadb9d59353436_thumbnail_560x.webp", 139.90, "M", 37),
+(2, "Camiseta Slim", "Camiseta canelada slim", "https://www.insiderstore.com.br/cdn/shop/files/TechT-ShirtHeavySlim_Preta_06_ddf53303-68c7-430f-9714-6526ffc20c36.jpg?v=1758647729&width=1206", 99.90, "P", 52);
+
+insert into ItemPedido (pedido_id, produto_id, quantidade) values 
+(1, 1, 1),
+(2, 1, 1),
+(2, 2, 1),
+(3, 2, 3);
